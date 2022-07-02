cb78b833febfbdf9b92560552cb5da0c9d5cc899
Prudhvi Karanam <prudhvikaranam537@gmail.com>
2022-07-02 23:36:16 +0530

    'index.html'

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        items: 3,
+        margin: 20,
+        dots: true,
+        dotsEach: true,
+        autoplay: false,
+        autoplayTimeout: 2000,
+        nav: true,
+        autoWidth: false
+    });
+});
