c916e445767d74d855993e79d4ed2e230f857bd7
Prudhvi Karanam <prudhvikaranam537@gmail.com>
2022-07-10 13:53:34 +0530

    closing navigation on click

--- script.js
+++ script.js
@@ -3,22 +3,25 @@ let outSideNavBar = document.getElementById('mobileNavigation');
 let closeIcon = document.getElementById('closeNav');
 getIcon.addEventListener('click', openNavContainer);
 closeIcon.addEventListener('click', closeNavContainer);
-window.addEventListener('click', outsideClick);
+// window.addEventListener('click', outsideClick);
 
 
 function openNavContainer() {
     outSideNavBar.style.display = "flex";
+    console.log(document.getElementsByTagName('body'));
+    document.getElementsByTagName('body')[0].style.overflow = 'hidden';
 }
 
 function closeNavContainer() {
     outSideNavBar.style.display = "none";
+    document.getElementsByTagName('body')[0].style.overflow = 'auto';
 }
 
-function outsideClick(e) {
-    if (e.target == outSideNavBar) {
-        outSideNavBar.style.display = "none";
-    }
-}
+// function outsideClick(e) {
+//     if (e.target == outSideNavBar) {
+//         outSideNavBar.style.display = "none";
+//     }
+// }
 
 
 $(document).ready(function() {
@@ -49,3 +52,51 @@ $(document).ready(function() {
         }
     });
 });
+
+
+
+function navigate(e) {
+    e.preventDefault();
+    let selectedNav = e.target.innerText.toLowerCase();
+    if (selectedNav.includes('home')) {
+        document.getElementById('navigator').scrollIntoView();
+    } else if (selectedNav.includes('services')) {
+        document.getElementById('servicess').scrollIntoView();
+    } else if (selectedNav.includes('about')) {
+        document.getElementById('about').scrollIntoView();
+    } else if (selectedNav.includes('review')) {
+        document.getElementById('reviews').scrollIntoView();
+    } else if (selectedNav.includes('contact')) {
+        document.getElementById('contact').scrollIntoView();
+    }
+    outSideNavBar.style.display = "none";
+    document.getElementsByTagName('body')[0].style.overflow = 'auto';
+}
+
+// for checking the current div
+
+document.addEventListener('scroll', (e) => {
+
+    let current = ''
+    let doc = document.querySelectorAll('section');
+
+    doc.forEach((d) => {
+        const secTop = d.offsetTop;
+        const sectionHeight = d.clientHeight;
+        if (scrollY > secTop) {
+            current = d.getAttribute('id');
+        }
+        // isInViewport(d)
+    })
+    console.log(current);
+
+})
+
+
+function isInViewport(el) {
+    // const rect = el.getBoundingClientRect();
+    // console.log(rect);
+    // return (
+    //     rect.top >= 0 || rect.bottom >= 0
+    // );
+}
